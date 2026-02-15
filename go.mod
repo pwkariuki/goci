41c593f247d11535b3246db4476dd1eb80af7c28
Patrick Kariuki <pkariuk1@jhu.edu>
2026-02-14 21:39:15 -0500

    executing external commands

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goci
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func run(proj string, out io.Writer) error {
+	if proj == "" {
+		return fmt.Errorf("Project directory is required")
+	}
+
+	// Build target project using `go build` to validate program correctness.
+	// Building multiple packages together ensures go build doesn't create an executable.
+	args := []string{"build", ".", "errors"}
+	cmd := exec.Command("go", args...)
+	cmd.Dir = proj // set working directory to project directory
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("'go build' failed: %s", err)
+	}
+	_, err := fmt.Fprintln(out, "Go Build: SUCCESS")
+	return err
+}
+
+func main() {
+	proj := flag.String("p", "", "Project directory")
+	flag.Parse()
+
+	if err := run(*proj, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
